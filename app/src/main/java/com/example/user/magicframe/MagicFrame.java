a1e1c1bd7d4f142089b294e40afec0dd2dfad1ee
simonibo <daswirknobe0603>
2017-06-17 20:53:24 +0200

    added class

---
+++ app/src/main/java/com/example/user/magicframe/MagicFrame.java
@@ -0,0 +1,15 @@
+package com.example.user.magicframe;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by user on 17.06.17.
+ * The main view of the app.
+ */
+
+public class MagicFrame extends View {
+    public MagicFrame(Context context) {
+        super(context);
+    }
+}
