cb77693e155f44394fa553d640f821e81c77fab3
BuildTools <unconfigured@null.spigotmc.org>
2017-06-19 19:01:46 +0200

    implemented sensors

--- app/src/main/java/com/example/user/magicframe/MagicFrame.java
+++ app/src/main/java/com/example/user/magicframe/MagicFrame.java
@@ -2,10 +2,13 @@ package com.example.user.magicframe;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
@@ -19,28 +22,55 @@ public class MagicFrame extends View implements SensorEventListener {
     private double x, y, vx, vy;
     public SensorManager sm;
     public Sensor as;
+    private long time;
+    private long lastlogtime;
+    Paint black;
 
     public MagicFrame(Context context) {
         super(context);
+        init();
+    }
+
+    public MagicFrame(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MagicFrame(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
         xmin = 0;
         xmax = getWidth();
         ymin = 0;
         ymax = getHeight();
-
-        if(sm != null) {
-            as = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        } else {
-            Log.d("Sensors", "Sensors ain't available");
-        }
+        black = new Paint();
+        black.setColor(Color.BLACK);
     }
 
     protected void onDraw(Canvas canvas) {
-
+        canvas.drawRect(0, 0, 100, 100, black);
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        
+        if(time == 0) {
+            time = sensorEvent.timestamp;
+            lastlogtime = time;
+        } else {
+            long dt = sensorEvent.timestamp - time;
+            vx += sensorEvent.values[0] * dt;
+            vy += sensorEvent.values[1] * dt;
+            x += vx * dt;
+            y += vy * dt;
+            time = sensorEvent.timestamp;
+            if(lastlogtime - time > 1000000000) {
+                Log.d("Position", x + ", " + y);
+                lastlogtime = time;
+            }
+        }
     }
 
     @Override
--- app/src/main/java/com/example/user/magicframe/MainActivity.java
+++ app/src/main/java/com/example/user/magicframe/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.user.magicframe;
 
 import android.content.Context;
+import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    MagicFrame mf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MagicFrame mf = (MagicFrame) findViewById(R.id.magicframe);
+        mf = (MagicFrame) findViewById(R.id.magicframe);
         mf.sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if(mf.sm != null) {
+            Sensor s = mf.sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            if(s != null) {
+                Log.d("Sensors", "Found sensor!");
+                mf.as = s;
+            } else {
+                Log.d("Sensors", "Linear acceleration sensor ain't available!");
+            }
+        } else {
+            Log.d("Sensors", "Sensors ain't available");
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mf.sm.registerListener(mf, mf.as, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mf.sm.unregisterListener(mf);
     }
 }
